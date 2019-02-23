692f9518f1995341402785bd26c07f5807bda8a5
Martin Eide <eidemartin_303@hotmail.com>
2019-02-23 17:29:25 +0100

    added section component

--- src/components/containers/section/Section.js
+++ src/components/containers/section/Section.js
@@ -1,10 +1,23 @@
 import React, {Component} from 'react';
 
+let sectionStyle = {
+    width: "100%",
+    backgroundColor: "#ffffff",
+    margin: "0 auto"
+}
+
 class Section extends Component {
-    
+    constructor(props) {
+        super(props);
+    }
+
     render() {
+
+        this.props.width ? sectionStyle.width = this.props.width : null;
+        this.props.backgroundColor ? sectionStyle.backgroundColor = this.props.backgroundColor : null;
+
         return(
-            <section>
+            <section style={sectionStyle}>
                 {this.props.children}
             </section>
         );
@@ -12,3 +25,10 @@ class Section extends Component {
 }
 
 export default Section;
+
+
+/*
+* Component is inteded to be used as a container for a section of webpage.
+* This component can take backgroundColor and width as props.
+* Will display the children within the section tag.
+*/
